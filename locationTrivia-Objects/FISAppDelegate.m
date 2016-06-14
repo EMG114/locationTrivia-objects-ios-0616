abf0602d197466dfddc8862b35adc97e61d2de07
EMG114 <egutierrez411@gmail.com>
2016-06-13 20:30:08 -0400

    done trivia

--- locationTrivia-Objects/FISAppDelegate.m
+++ locationTrivia-Objects/FISAppDelegate.m
@@ -1,6 +1,7 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISLocation.h"
 
 @implementation FISAppDelegate
 
@@ -8,5 +9,53 @@
     // Override point for customization after application launch.
     return YES;
 }
+
+
+
+
+
+- (NSArray*) allLocationNames{
+    
+    NSMutableArray *locationNames = [[NSMutableArray alloc]init];
+    
+    for (FISLocation *location in self.locations) {
+        
+        [locationNames addObject:location.name];
+    }
+    
+    return locationNames;
+}
+
+- (FISLocation*) locationNamed:(NSString *)name{
+    
+    for (FISLocation *location in self.locations) {
+        
+        if ([location.name isEqualToString:name]) {
+            
+            return location;
+        }
+    }
+    return nil;
+    
+}
+
+- (NSArray*) locationsNearLatitude:(CGFloat)latitude
+                         longitude:(CGFloat)longitude
+                            margin:(CGFloat)margin{
+    
+    NSMutableArray *locationsInRange = [[NSMutableArray alloc] init];
+    
+    for (FISLocation *location in self.locations) {
+        
+        if (location.latitude <= latitude + margin && location.latitude >= latitude - margin && location.longitude <= longitude + margin && location.longitude >= longitude - margin) {
+            
+            [locationsInRange addObject:location];
+        }
+    }
+    
+    return locationsInRange;
+}
+
+
 
 @end
